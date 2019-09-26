e7faf3728316e4713164ac6611244cfbf57ac82e
Steven Bowman <stevenbowmanjr@gmail.com>
2019-09-25 21:40:50 -0700

    added a data state property then copied the on click function from assignment 15 pausing gifs, needs styling but complete other than that

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -48,10 +48,11 @@ $(document).ready(function () {
             console.log(response)
             for (let i = 0; i < 12; i++) {
                 let gifUrl = response.data[i].images.fixed_height.url;
-                let gifImg = $("<img>");
+                let gifImg = $("<img class='gif'>");
                 gifImg.attr("src", gifUrl);
                 gifImg.attr("data-animate", response.data[i].images.fixed_height.url);
-                    gifImg.attr("data-still", response.data[i].images.fixed_height_still.url)
+                gifImg.attr("data-still", response.data[i].images.fixed_height_still.url)
+                gifImg.attr("data-state", "animate")
                 $("#gifDisplay").append(gifImg)
             }
         })
@@ -71,7 +72,24 @@ $(document).ready(function () {
 
     })
 
-    
+    $("#gifDisplay").on("click", ".gif", function() {
+        var state = $(this).attr('data-state')
+      // CODE GOES HERE
+      console.log($(this).attr('data-state'));
+      var animatedGif = $(this).attr('data-animate')
+      console.log($(this).attr('data-animate'))
+      var stillGif = $(this).attr('data-still')
+      console.log(stillGif)
 
+      if (state == "still"){
+        $(this).attr('src', animatedGif)
+        $(this).attr('data-state', "animate")
+        
+        
+      } else if (state == "animate"){
+        $(this).attr('data-state', "still")
+        $(this).attr('src', stillGif)}
+
+    });
 
 });
